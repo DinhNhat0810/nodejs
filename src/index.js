dfca26267ff05455ba0b19fe2cd6d40d2aa7bf7b
DinhNhat0810 <dinhnhat0810@gmail.com>
2022-01-03 10:08:36 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+//--------------------------------------------//
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ extname: '.hbs' });
+
+//Routes
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+// TEMPLATE ENGINE
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './resources/views'));
+
+//HTTP logger
+app.use(morgan('combined'));
+
+//Route init
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
