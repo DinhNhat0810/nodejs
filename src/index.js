8e2abd46a20ff95ce950c929cd4bd09951b88254
DinhNhat0810 <dinhnhat0810@gmail.com>
2022-01-14 14:15:35 +0700

    A

--- src/index.js
+++ src/index.js
@@ -1,36 +1,51 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const path = require('path')
+const app = express()
 const port = 3000;
 
 //--------------------------------------------//
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ extname: '.hbs' });
+const handlebars = require('express-handlebars')
+
 
 //Routes
-const route = require('./routes');
+const route = require('./routes')
+const db = require('./config/db')
 
-app.use(express.static(path.join(__dirname, 'public')));
+//Connect database
+db.connect()
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(
     express.urlencoded({
         extended: true,
     }),
 );
-app.use(express.json());
+app.use(express.json())
 
-// TEMPLATE ENGINE
-app.engine('hbs', hbs.engine);
+// Template engine
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
+    }),
+)
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, './resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'))
+
+app.use(methodOverride('_method'))
 
 //HTTP logger
-app.use(morgan('combined'));
+app.use(morgan('combined'))
 
 //Route init
-route(app);
+route(app)
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`App listening at http://localhost:${port}`)
+})
